92271cc952dbc45e3f0a75716965d50f3e34bee1
RaphMt <rafa.mtorres117@outlook.com>
2023-12-16 22:14:13 -0300

    rewrite of none-ls, cmp and lsp configs

--- lua/raphmt/plugins/none-ls.lua
+++ lua/raphmt/plugins/none-ls.lua
@@ -1,67 +1,102 @@
+-- linters and formatter
 return {
-
-	-- code actions, diagnostics and completion within LSP (null-ls, maintained by the community)
+	-- "nvimtools/none-ls.nvim",
+	-- lazy = true,
+	-- event = { "BufReadPre", "BufNewFile" },
+	-- dependencies = {
+	-- 	"jay-babu/mason-null-ls.nvim",
+	-- },
+	-- config = function()
+	-- 	local mason_null_ls = require("mason-null-ls")
+	-- 	local null_ls = require("null-ls")
+	-- 	local null_ls_utils = require("null-ls.utils")
+	--
+	-- 	mason_null_ls.setup({
+	-- 		ensure_installed = {
+	-- 			"prettier", -- prettier formatter
+	-- 			"stylua", -- lua formatter
+	-- 			"eslint_d", -- js linter
+	-- 		},
+	-- 	})
+	--
+	-- 	-- for conciseness
+	-- 	local formatting = null_ls.builtins.formatting -- to setup formatters
+	-- 	local diagnostics = null_ls.builtins.diagnostics -- to setup linters
+	--
+	-- 	-- to setup format on save
+	-- 	local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+	--
+	-- 	-- configure null_ls
+	-- 	null_ls.setup({
+	-- 		-- add package.json as identifier for root (for typescript monorepos)
+	-- 		root_dir = null_ls_utils.root_pattern(".null-ls-root", "Makefile", ".git", "package.json"),
+	--
+	-- 		-- setup formatters & linters
+	-- 		sources = {
+	-- 			--  to disable file types use
+	-- 			--  "formatting.prettier.with({disabled_filetypes: {}})" (see null-ls docs)
+	--
+	-- 			formatting.prettier, -- js/ts formatter
+	-- 			formatting.stylua, -- lua formatter
+	-- 			diagnostics.eslint_d.with({ -- js/ts linter
+	-- 				condition = function(utils)
+	-- 					return utils.root_has_file({ ".eslintrc.js", ".eslintrc.cjs" }) -- only enable if root has .eslintrc.js or .eslintrc.cjs
+	-- 				end,
+	-- 			}),
+	-- 		},
+	--
+	-- 		-- configure format on save
+	-- 		on_attach = function(current_client, bufnr)
+	-- 			if current_client.supports_method("textDocument/formatting") then
+	-- 				vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+	-- 				vim.api.nvim_create_autocmd("BufWritePre", {
+	-- 					group = augroup,
+	-- 					buffer = bufnr,
+	-- 					callback = function()
+	-- 						vim.lsp.buf.format({
+	-- 							filter = function(client)
+	-- 								--  only use null-ls for formatting instead of lsp server
+	-- 								return client.name == "null-ls"
+	-- 							end,
+	-- 							bufnr = bufnr,
+	-- 						})
+	-- 					end,
+	-- 				})
+	-- 			end
+	-- 		end,
+	-- 	})
+	-- end,
 	{
-		"nvimtools/none-ls.nvim", -- configure formatters & linters
-		event = { "BufReadPre", "BufNewFile" }, -- to enable uncomment this
-		dependencies = {
-			"jay-babu/mason-null-ls.nvim",
-		},
-		config = function()
-			local mason_null_ls = require("mason-null-ls")
-
+		"nvimtools/none-ls.nvim",
+		event = "VeryLazy",
+		dependencies = { "nvim-lua/plenary.nvim", "jay-babu/mason-null-ls.nvim" },
+		opts = function()
+			local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 			local null_ls = require("null-ls")
 
-			local null_ls_utils = require("null-ls.utils")
-
-			mason_null_ls.setup({
-				ensure_installed = {
-					"prettier", -- prettier formatter
-					"stylua", -- lua formatter
-					"eslint_d", -- js linter
-				},
-			})
-			-- for conciseness
-			local formatting = null_ls.builtins.formatting -- to setup formatters
-			local diagnostics = null_ls.builtins.diagnostics -- to setup linters
-			-- to setup format on save
-			local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
-
-			-- configure null_ls
-			null_ls.setup({
-				-- add package.json as identifier for root (for typescript monorepos)
-				root_dir = null_ls_utils.root_pattern(".null-ls-root", "Makefile", ".git", "package.json"),
-				-- setup formatters & linters
+			local opts = {
 				sources = {
-					--  to disable file types use
-					--  "formatting.prettier.with({disabled_filetypes: {}})" (see null-ls docs)
-					formatting.prettier, -- js/ts formatter
-					diagnostics.eslint_d.with({ -- js/ts linter
-						condition = function(utils)
-							return utils.root_has_file({ ".eslintrc.js", ".eslintrc.cjs" }) -- only enable if root has .eslintrc.js or .eslintrc.cjs
-						end,
-					}),
+					null_ls.builtins.diagnostics.eslint,
+					null_ls.builtins.formatting.prettier,
 				},
-				-- configure format on save
-				on_attach = function(current_client, bufnr)
-					if current_client.supports_method("textDocument/formatting") then
-						vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+				on_attach = function(client, bufnr)
+					if client.supports_method("textDocument/formatting") then
+						vim.api.nvim_clear_autocmds({
+							group = augroup,
+							buffer = bufnr,
+						})
 						vim.api.nvim_create_autocmd("BufWritePre", {
 							group = augroup,
 							buffer = bufnr,
 							callback = function()
-								vim.lsp.buf.format({
-									filter = function(client)
-										--  only use null-ls for formatting instead of lsp server
-										return client.name == "null-ls"
-									end,
-									bufnr = bufnr,
-								})
+								vim.lsp.buf.format({ bufnr = bufnr })
 							end,
 						})
 					end
 				end,
-			})
+			}
+
+			return opts
 		end,
 	},
 }
