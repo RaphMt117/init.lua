47fc7dada8c76b20a534eb243c458847a756b57d
RaphMt <rafa.mtorres117@outlook.com>
2024-01-24 16:24:19 -0300

    changed markdown formatter

--- lua/raphmt/plugins/conform.lua
+++ lua/raphmt/plugins/conform.lua
@@ -9,7 +9,7 @@ return {
 			require("conform").setup({
 				formatters_by_ft = {
 					lua = { "stylua" },
-					markdown = { "markdownlint" },
+					markdown = { "markdown-toc" },
 				},
 
 				format_on_save = function(bufnr)
