cadc72ae0dcbb5c84081745c07d53e4b2aefeaa4
RaphMt117 <rafa.mtorres117@outlook.com>
2025-01-12 01:26:44 -0300

    abbreviations, utilities methods

--- lua/raphmt/init.lua
+++ lua/raphmt/init.lua
@@ -2,6 +2,8 @@ require("raphmt.lazy")
 require("raphmt.set")
 require("raphmt.mappings")
 require("raphmt.autocmds")
+require("raphmt.util.utils")
+require("raphmt.util.abbreviations")
 
 -- vim.g.netrw_browse_split = 0
 -- vim.g.netrw_banner = 0
---
+++ lua/raphmt/util/abbreviations.lua
@@ -0,0 +1,18 @@
+-- inoreabbrev (Insert mode abbreviations)
+-- iabbrev (Insert and Command-line mode abbreviations)
+
+vim.cmd([[
+" pt-br
+inoreabbrev hj hoje
+inoreabbrev tambem também
+inoreabbrev voce você
+inoreabbrev codigo código
+
+" en-us
+inoreabbrev dont don't
+inoreabbrev btw by the way
+inoreabbrev im I'm
+inoreabbrev youre you're
+
+" code
+]])
