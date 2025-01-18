d353d0777a19d48fda4267986b428110d4727226
RaphMt117 <rafa.mtorres117@outlook.com>
2025-01-17 22:49:45 -0300

    removed manual go format

--- lua/raphmt/lua/config/autocmds.lua
+++ lua/raphmt/lua/config/autocmds.lua
@@ -135,10 +135,10 @@ autocmd("FileType", {
 -- Run gofmt + goimport on save
 -- local goimport_sync_grp = autocmd("GoImport", {})
 
-vim.api.nvim_create_autocmd("BufWritePre", {
-	pattern = "*.go",
-	callback = function()
-		require("go.format").goimport()
-	end,
-	group = goimport_sync_grp,
-})
+-- vim.api.nvim_create_autocmd("BufWritePre", {
+-- 	pattern = "*.go",
+-- 	callback = function()
+-- 		require("go.format").goimport()
+-- 	end,
+-- 	group = goimport_sync_grp,
+-- })
