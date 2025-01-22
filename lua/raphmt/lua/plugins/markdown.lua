1d5fca06db4ffc42c7630ac609d99e82fdcb8738
RaphMt117 <rafa.mtorres117@outlook.com>
2025-01-22 16:46:25 -0300

    added harpoon, markdown highlight and tailwind color support

--- lua/raphmt/lua/plugins/markdown.lua
+++ lua/raphmt/lua/plugins/markdown.lua
@@ -1,50 +1,126 @@
 return {
-	{
-		"iamcco/markdown-preview.nvim",
-		cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-		build = function()
-			vim.fn["mkdp#util#install"]()
-		end,
-		config = function()
-			vim.cmd([[do FileType]])
-		end,
+	"MeanderingProgrammer/render-markdown.nvim",
+	enabled = true,
+	dependencies = {
+		"nvim-treesitter/nvim-treesitter",
+		"nvim-tree/nvim-web-devicons",
 	},
+	---@module 'render-markdown'
+	-- ft = { "markdown", "norg", "rmd", "org" },
+	init = function()
+		-- Define colors
+		local color1_bg = "#ff757f"
+		local color2_bg = "#4fd6be"
+		local color3_bg = "#7dcfff"
+		local color4_bg = "#ff9e64"
+		local color5_bg = "#7aa2f7"
+		local color6_bg = "#c0caf5"
+		local color_fg = "#1F2335"
+		-- -- Heading background
+		vim.cmd(string.format([[highlight Headline1Bg guifg=%s guibg=%s gui=bold]], color_fg, color1_bg))
+		vim.cmd(string.format([[highlight Headline2Bg guifg=%s guibg=%s gui=bold]], color_fg, color2_bg))
+		vim.cmd(string.format([[highlight Headline3Bg guifg=%s guibg=%s gui=bold]], color_fg, color3_bg))
+		vim.cmd(string.format([[highlight Headline4Bg guifg=%s guibg=%s gui=bold]], color_fg, color4_bg))
+		vim.cmd(string.format([[highlight Headline5Bg guifg=%s guibg=%s gui=bold]], color_fg, color5_bg))
+		vim.cmd(string.format([[highlight Headline6Bg guifg=%s guibg=%s gui=bold]], color_fg, color6_bg))
 
-	{
-		"lukas-reineke/headlines.nvim",
-		opts = function()
-			local opts = {}
-			for _, ft in ipairs({
-				"markdown", --[[ "norg", ]]
-				"rmd",
-				"org",
-			}) do
-				opts[ft] = {
-					headline_highlights = {},
-					-- disable bullets for now. See https://github.com/lukas-reineke/headlines.nvim/issues/66
-					bullets = {},
-				}
-				for i = 1, 6 do
-					local hl = "Headline" .. i
-					vim.api.nvim_set_hl(0, hl, { link = "Headline", default = true })
-					table.insert(opts[ft].headline_highlights, hl)
-				end
-			end
-			return opts
-		end,
-		ft = {
-			"markdown", --[[ "norg", ]]
-			"rmd",
-			"org",
+		-- Heading fg
+		-- vim.cmd(string.format([[highlight Headline1Fg guifg=%s gui=bold]], colors.color1_bg))
+		-- vim.cmd(string.format([[highlight Headline2Fg guifg=%s gui=bold]], colors.color2_bg))
+		-- vim.cmd(string.format([[highlight Headline3Fg guifg=%s gui=bold]], colors.color3_bg))
+		-- vim.cmd(string.format([[highlight Headline4Fg guifg=%s gui=bold]], colors.color4_bg))
+		-- vim.cmd(string.format([[highlight Headline5Fg guifg=%s gui=bold]], colors.color5_bg))
+		-- vim.cmd(string.format([[highlight Headline6Fg guifg=%s gui=bold]], colors.color6_bg))
+	end,
+	opts = {
+		heading = {
+			sign = false,
+			icons = { "󰎤 ", "󰎧 ", "󰎪 ", "󰎭 ", "󰎱 ", "󰎳 " },
+			backgrounds = {
+				"Headline1Bg",
+				"Headline2Bg",
+				"Headline3Bg",
+				"Headline4Bg",
+				"Headline5Bg",
+				"Headline6Bg",
+			},
+			foregrounds = {
+				"Headline1Fg",
+				"Headline2Fg",
+				"Headline3Fg",
+				"Headline4Fg",
+				"Headline5Fg",
+				"Headline6Fg",
+			},
 		},
-		config = function(_, opts)
-			vim.schedule(function()
-				require("headlines").setup(opts)
-				require("headlines").refresh()
-			end)
-		end,
+		code = {
+			sign = false,
+			width = "block",
+			right_pad = 1,
+		},
+		bullet = {
+			-- Turn on / off list bullet rendering
+			enabled = true,
+		},
+		checkbox = {
+			-- Turn on / off checkbox state rendering
+			enabled = true,
+			-- Determines how icons fill the available space:
+			--  inline:  underlying text is concealed resulting in a left aligned icon
+			--  overlay: result is left padded with spaces to hide any additional text
+			position = "inline",
+			unchecked = {
+				-- Replaces '[ ]' of 'task_list_marker_unchecked'
+				icon = "   󰄱 ",
+				-- Highlight for the unchecked icon
+				highlight = "RenderMarkdownUnchecked",
+				-- Highlight for item associated with unchecked checkbox
+				scope_highlight = nil,
+			},
+			checked = {
+				-- Replaces '[x]' of 'task_list_marker_checked'
+				icon = "   󰱒 ",
+				-- Highlight for the checked icon
+				highlight = "RenderMarkdownChecked",
+				-- Highlight for item associated with checked checkbox
+				scope_highlight = nil,
+			},
+		},
 	},
-
+	-- {
+	-- 	"lukas-reineke/headlines.nvim",
+	-- 	opts = function()
+	-- 		local opts = {}
+	-- 		for _, ft in ipairs({
+	-- 			"markdown", --[[ "norg", ]]
+	-- 			"rmd",
+	-- 			"org",
+	-- 		}) do
+	-- 			opts[ft] = {
+	-- 				headline_highlights = {},
+	-- 				-- disable bullets for now. See https://github.com/lukas-reineke/headlines.nvim/issues/66
+	-- 				bullets = {},
+	-- 			}
+	-- 			for i = 1, 6 do
+	-- 				local hl = "Headline" .. i
+	-- 				vim.api.nvim_set_hl(0, hl, { link = "Headline", default = true })
+	-- 				table.insert(opts[ft].headline_highlights, hl)
+	-- 			end
+	-- 		end
+	-- 		return opts
+	-- 	end,
+	-- 	ft = {
+	-- 		"markdown", --[[ "norg", ]]
+	-- 		"rmd",
+	-- 		"org",
+	-- 	},
+	-- 	config = function(_, opts)
+	-- 		vim.schedule(function()
+	-- 			require("headlines").setup(opts)
+	-- 			require("headlines").refresh()
+	-- 		end)
+	-- 	end,
+	-- },
 	{
 		"mzlogin/vim-markdown-toc",
 	},
