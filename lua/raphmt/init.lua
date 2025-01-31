1bc7e69938c50f28e78b23e535c3c9c3b6a2c57e
RaphMt117 <rafa.mtorres117@outlook.com>
2025-01-30 22:07:48 -0300

    codium plugin

--- lua/raphmt/init.lua
+++ lua/raphmt/init.lua
@@ -1,5 +1,10 @@
-require("raphmt.lua.config.options")
-require("raphmt.lua.config.keymaps")
-require("raphmt.lua.config.autocmds")
-require("raphmt.lua.config.abbreviations")
-require("raphmt.lua.config.lazy")
+if vim.g.vscode then
+	require("raphmt.lua.config.code.options")
+	require("raphmt.lua.config.code.keymaps")
+else
+	require("raphmt.lua.config.options")
+	require("raphmt.lua.config.keymaps")
+	require("raphmt.lua.config.autocmds")
+	require("raphmt.lua.config.abbreviations")
+	require("raphmt.lua.config.lazy")
+end
