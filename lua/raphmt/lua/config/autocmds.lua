53d3a6c0088d0208e3623d25dc725dbca2eda4f2
RaphMt117 <rafa.mtorres117@outlook.com>
2025-02-09 22:23:51 -0300

    removed unused stuff

--- lua/raphmt/lua/config/autocmds.lua
+++ lua/raphmt/lua/config/autocmds.lua
@@ -24,8 +24,7 @@ autocmd("TextYankPost", {
 	end,
 })
 
--- go to last loc when opening a buffer
--- this mean that when you open a file, you will be at the last position
+-- when opening a file, return at the last cursor position
 autocmd("BufReadPost", {
 	callback = function()
 		local mark = vim.api.nvim_buf_get_mark(0, '"')
@@ -33,56 +32,6 @@ autocmd("BufReadPost", {
 		if mark[1] > 0 and mark[1] <= lcount then
 			pcall(vim.api.nvim_win_set_cursor, 0, mark)
 		end
-	end,
-})
-
--- auto close brackets
--- this
-autocmd("FileType", { pattern = "man", command = [[nnoremap <buffer><silent> q :quit<CR>]] })
-
--- Enable spell checking for certain file types
--- api.nvim_create_autocmd(
---   { "BufRead", "BufNewFile" },
---   {
---     pattern = { "*.txt", "*.md", "*.tex" },
---     callback = function()
---       vim.opt.spell = true
---       vim.opt.spelllang = "en"
---     end,
---   }
--- )
-
-autocmd("ColorScheme", {
-	callback = function()
-		-- change the background color of floating windows and borders.
-		-- vim.cmd('highlight NormalFloat guibg=none guifg=none')
-		-- vim.cmd('highlight FloatBorder guifg=' .. colors.fg .. ' guibg=none')
-		-- vim.cmd('highlight NormalNC guibg=none guifg=none')
-
-		-- vim.cmd('highlight TelescopeBorder guifg=' .. colors.fg .. ' guibg=none')
-		-- vim.cmd('highlight TelescopePromptBorder guifg=' .. colors.fg .. ' guibg=none')
-		-- vim.cmd('highlight TelescopeResultsBorder guifg=' .. colors.fg .. ' guibg=none')
-		--
-		-- vim.cmd('highlight TelescopePromptTitle guifg=' .. colors.fg .. ' guibg=none')
-		-- vim.cmd('highlight TelescopeResultsTitle guifg=' .. colors.fg .. ' guibg=none')
-		-- vim.cmd('highlight TelescopePreviewTitle guifg=' .. colors.fg .. ' guibg=none')
-		--
-
-		-- change neotree background colors
-		-- Default: NeoTreeNormal  xxx ctermfg=223 ctermbg=232 guifg=#d4be98 guibg=#141617
-		-- vim.cmd('highlight NeoTreeNormal guibg=#252e33 guifg=none')
-		-- vim.cmd('highlight NeoTreeFloatNormal guifg=none guibg=none')
-		-- vim.cmd('highlight NeoTreeFloatBorder gui=none guifg=' .. colors.fg .. ' guibg=none')
-		-- vim.cmd('highlight NeoTreeEndOfBuffer guibg=#252e33') -- 1d2021
-
-		vim.api.nvim_set_hl(0, "FloatBorder", { link = "Normal" })
-		vim.api.nvim_set_hl(0, "LspInfoBorder", { link = "Normal" })
-		vim.api.nvim_set_hl(0, "NormalFloat", { link = "Normal" })
-
-		vim.cmd("highlight Winbar guibg=none")
-
-		-- vim.cmd("highlight Comment guifg=#475558")
-		-- vim.cmd("highlight Comment guifg=green")
 	end,
 })
 
@@ -113,7 +62,7 @@ autocmd("FileType", {
 -- resize neovim split when terminal is resized
 command("autocmd VimResized * wincmd =")
 
--- fix terraform and hcl comment string
+-- fix terraform and hcl comment string to use #
 autocmd("FileType", {
 	group = vim.api.nvim_create_augroup("FixTerraformCommentString", { clear = true }),
 	callback = function(ev)
