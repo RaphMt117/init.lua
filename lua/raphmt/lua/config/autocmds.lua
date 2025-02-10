8d76f0aacbf5bef1dc1b67bc869b37cf6a174695
RaphMt117 <rafa.mtorres117@outlook.com>
2025-02-09 22:30:25 -0300

    removed more unused stuff

--- lua/raphmt/lua/config/autocmds.lua
+++ lua/raphmt/lua/config/autocmds.lua
@@ -35,6 +35,19 @@ autocmd("BufReadPost", {
 	end,
 })
 
+-- q to close man pages
+autocmd("FileType", { pattern = "man", command = [[nnoremap <buffer><silent> q :quit<CR>]] })
+
+autocmd("ColorScheme", {
+	callback = function()
+		vim.api.nvim_set_hl(0, "FloatBorder", { link = "Normal" })
+		vim.api.nvim_set_hl(0, "LspInfoBorder", { link = "Normal" })
+		vim.api.nvim_set_hl(0, "NormalFloat", { link = "Normal" })
+
+		vim.cmd("highlight Winbar guibg=none")
+	end,
+})
+
 -- close some filetypes with <q>
 autocmd("FileType", {
 	group = vim.api.nvim_create_augroup("close_with_q", { clear = true }),
