56df9588087f09cd5564e4be804518b4f02384c2
RaphMt117 <rafa.mtorres117@outlook.com>
2025-02-09 22:17:47 -0300

    add comments

--- lua/raphmt/init.lua
+++ lua/raphmt/init.lua
@@ -1,7 +1,9 @@
 if vim.g.vscode then
+	-- vscode/codium extension specific config
 	require("raphmt.lua.config.code.options")
 	require("raphmt.lua.config.code.keymaps")
 else
+	-- neovim specific config
 	require("raphmt.lua.config.options")
 	require("raphmt.lua.config.keymaps")
 	require("raphmt.lua.config.autocmds")
